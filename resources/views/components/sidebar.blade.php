736ad1007deb6b2a3808492be6519f0c4f28060f
Code IT <codeit.np@gmail.com>
2024-03-15 20:10:00 +0545

    initial commit

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,25 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html"> <img alt="image" src="/assets/img/logo.png" class="header-logo" />
+                <span class="logo-name">Admin</span>
+            </a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Main</li>
+            <li class="dropdown active">
+                <a href="index.html" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
+            </li>
+            <li class="dropdown">
+                <a href="index.html" class="nav-link"><i data-feather="home"></i><span>Company</span></a>
+            </li>
+            <li class="dropdown">
+                <a href="index.html" class="nav-link"><i data-feather="grid"></i><span>Category</span></a>
+            </li>
+            <li class="dropdown">
+                <a href="index.html" class="nav-link"><i data-feather="tv"></i><span>Post</span></a>
+            </li>
+
+        </ul>
+    </aside>
+</div>
